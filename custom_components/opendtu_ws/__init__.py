0cedb38e8cefa22288cf1ba68ffb5be8e7ef21d4
jonnycastaway <jonny.castaway@gmx.net>
2026-02-17 16:25:32 +0100

    Delete custom_components/opendtu_ws directory

--- custom_components/opendtu_ws/__init__.py
+++
@@ -1,8 +0,0 @@
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return True
