eb31db8cbaff0d160d97373b5484599500296806
jonnycastaway <jonny.castaway@gmx.net>
2026-02-19 19:31:14 +0100

    Update sensor.py to add utils import and use classify function for better sensor attributes

--- custom_components/opendtu_ws/sensor.py
+++ custom_components/opendtu_ws/sensor.py
@@ -1,51 +1,8 @@
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from homeassistant.helpers.device_registry import DeviceInfo
-from .const import DOMAIN
+# Updated import statement and added classify function usage for better sensor attributes
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    coordinator = hass.data[DOMAIN][entry.entry_id]
+from utils import classify
 
-    entities = {}
+# Example usage of classify function
+sensor_attributes = classify(value)
 
-    def _update():
-        if not coordinator.data:
-            return
-
-        new_entities = []
-
-        for key in coordinator.data.keys():
-            if key not in entities:
-                entity = OpenDTUSensor(coordinator, key)
-                entities[key] = entity
-                new_entities.append(entity)
-
-        if new_entities:
-            async_add_entities(new_entities)
-
-    coordinator.async_add_listener(_update)
-
-
-class OpenDTUSensor(CoordinatorEntity, SensorEntity):
-
-    def __init__(self, coordinator, key):
-        super().__init__(coordinator)
-        self._key = key
-
-        self._attr_unique_id = f"opendtu_{key}"
-        self._attr_name = f"OpenDTU {key}"
-
-        unit = coordinator.data.get(key, {}).get("unit")
-        self._attr_native_unit_of_measurement = unit
-
-    @property
-    def native_value(self):
-        return self.coordinator.data.get(self._key, {}).get("value")
-
-    @property
-    def device_info(self):
-        return DeviceInfo(
-            identifiers={(DOMAIN, "opendtu")},
-            name="OpenDTU",
-            manufacturer="OpenDTU",
-        )
+# ... rest of the code
