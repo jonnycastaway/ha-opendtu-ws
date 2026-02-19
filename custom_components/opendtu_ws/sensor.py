286c47555f43b5dba18ccdfced1904f1978f1ad9
jonnycastaway <jonny.castaway@gmx.net>
2026-02-19 19:51:40 +0100

    Delete custom_components/opendtu_ws directory

--- custom_components/opendtu_ws/sensor.py
+++
@@ -1,8 +0,0 @@
-# Updated import statement and added classify function usage for better sensor attributes
-
-from utils import classify
-
-# Example usage of classify function
-sensor_attributes = classify(value)
-
-# ... rest of the code
